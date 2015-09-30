8932069d5106583ab5e539b6eaa36f3fcd833c06
booirror <booirror@gmail.com>
2015-09-30 23:49:47 +0800

    num

---
+++ algorithm/260single-number-iii.cpp
@@ -0,0 +1,11 @@
+vector<int> singleNumber(vector<int>& nums) {
+    int aXorb = 0; 
+    for (auto item : nums) aXorb ^= item;
+    int lastBit = (aXorb & (aXorb - 1)) ^ aXorb;
+    int intA = 0, intB = 0;
+    for (auto item : nums) {
+        if (item & lastBit) intA = intA ^ item;
+        else intB = intB ^ item;
+    }
+    return vector<int>{intA, intB};   
+}
