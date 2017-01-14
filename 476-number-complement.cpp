123c73196b18c19e0dbb9aa9566efb9463bdad4c
booirror <booirror@gmail.com>
2017-01-14 23:55:33 +0800

    s
    s

---
+++ 476-number-complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 0xffffffff;
+        while (num & mask)
+        {
+            mask <<= 1;
+        }
+        return ~(num | mask);
+    }
+};
